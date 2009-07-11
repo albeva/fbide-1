0c6306281ca46a14442ca3e343a3893ea5bf5f0f
vongodric <vongodric@957c6b5c-1c3a-0410-895f-c76cfc11fbc7>
2009-07-11 15:24:04 +0000

    git-svn-id: http://svn.code.sf.net/p/fbide/code/trunk@197 957c6b5c-1c3a-0410-895f-c76cfc11fbc7

--- fbide-wx/App/include/version.h
+++ fbide-wx/App/include/version.h
@@ -16,17 +16,17 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR = 0;
 	static const long MINOR = 10;
-	static const long BUILD = 506;
-	static const long REVISION = 2845;
+	static const long BUILD = 507;
+	static const long REVISION = 2850;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT = 3738;
-	#define RC_FILEVERSION 0,10,506,2845
-	#define RC_FILEVERSION_STRING "0, 10, 506, 2845\0"
-	static const char FULLVERSION_STRING[] = "0.10.506.2845";
+	static const long BUILDS_COUNT = 3740;
+	#define RC_FILEVERSION 0,10,507,2850
+	#define RC_FILEVERSION_STRING "0, 10, 507, 2850\0"
+	static const char FULLVERSION_STRING[] = "0.10.507.2850";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY = 8;
+	static const long BUILD_HISTORY = 9;
 	
 
 }
