ab12834ac395384068274e5489d02e0351f827e1
vongodric <vongodric@957c6b5c-1c3a-0410-895f-c76cfc11fbc7>
2009-09-03 18:22:39 +0000

    git-svn-id: http://svn.code.sf.net/p/fbide/code/trunk@207 957c6b5c-1c3a-0410-895f-c76cfc11fbc7

--- fbide-rw/app/fbide.cpp
+++ fbide-rw/app/fbide.cpp
@@ -45,12 +45,14 @@ class FBIde : public wxApp
         // load ui
         GET_UIMGR()->Load();
 
+        /*
         if (argc > 1)
         {
             auto sp = this->GetTraits()->GetStandardPaths();
             auto path = ::wxPathOnly(sp.GetExecutablePath());
             GET_SCRIPTMGR()->Execute(path + "/" + argv[1]);
         }
+        */
 
         return true;
     }
